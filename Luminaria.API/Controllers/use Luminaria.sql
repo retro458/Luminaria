2362f0ac9dbcece91e09df24371d70133613575c
Elliot_1 <64877695+retro458@users.noreply.github.com>
2026-06-14 17:00:20 -0600

    feat:pruebas para verificar que el sp devueve las categorias sin importar si el dato es null en el sp

--- Luminaria.API/Controllers/use Luminaria.sql
+++ Luminaria.API/Controllers/use Luminaria.sql
@@ -2,3 +2,25 @@ use Luminaria
 
 alter table Auth.Usuarios
 alter column ContraseñaHash nvarchar(500) not null
+
+alter table Personajes
+alter column FechaFallecimiento date null -- se cambia el tipo de dato
+
+
+EXEC sp_ObtenerCatálogoPublico @Categoria = 'Programacion', @Busqueda = 'Ada';
+
+
+select * from Categorias
+
+CREATE FULLTEXT CATALOG ft_LuminariaCat AS DEFAULT;
+GO
+
+CREATE FULLTEXT INDEX ON Personajes(Nombre)
+KEY INDEX PK__Personaj__FF7028BD8DF74C0A
+WITH STOPLIST = SYSTEM; -- Ignora artículos comunes automáticamente (el, la, los)
+GO
+
+SELECT name 
+FROM sys.key_constraints 
+WHERE type = 'PK' AND parent_object_id = OBJECT_ID('dbo.Personajes');
+EXEC sp_ObtenerCatálogoPublico @Categoria = NULL, @Busqueda = 'Ad';
