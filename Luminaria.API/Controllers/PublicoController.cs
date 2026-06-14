4821dbcc0491fbeccb7c41b17388cbfecfb10c04
Elliot_1 <64877695+retro458@users.noreply.github.com>
2026-06-14 17:09:00 -0600

    feat:pruebas para verificar que el sp devueve las categorias sin importar si el dato es null en el sp

--- Luminaria.API/Controllers/PublicoController.cs
+++ Luminaria.API/Controllers/PublicoController.cs
@@ -43,7 +43,7 @@ namespace Luminaria.API.Controllers
 
             // 3. Ejecutar y leer ambos resultsets en un solo viaje a la BD
             using (var multi = await conexion.QueryMultipleAsync(
-                "EXEC [dbo].[sp_ObtenerCatálogoPublico] @Categoria, @Busqueda", 
+                "[dbo].[sp_ObtenerCatálogoPublico]", 
                 parametros, 
                 commandType: CommandType.StoredProcedure))
             {
